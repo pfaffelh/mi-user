03d58d8d95133d81c5b6fcb8bc64964dd29ff9ed
pfaffelh <p.p@stochastik.uni-freiburg.de>
2024-03-06 12:38:31 +0100

    initial commit

---
+++ misc/import.py
@@ -0,0 +1,84 @@
+from pymongo import MongoClient
+
+# This is the mongodb
+cluster = MongoClient("mongodb://127.0.0.1:27017")
+mongo_db = cluster["user"]
+group = mongo_db["group"]
+user = mongo_db["users"]
+
+groups = [
+     { "name": "admin",
+       "kommentar": "darf alles",
+       "rang": 10
+      },
+      { "name": "faq",
+       "kommentar": "Darf FAQs bearbeiten",
+       "rang": 20
+      },
+      { "name": "vvz",
+       "kommentar": "Darf vvz bearbeiten",
+       "rang": 20
+      }]
+
+users = [
+    {"name": "Pfaffelhuber",
+     "vorname": "Peter",
+     "rz": "pp131",
+     "email": "p.p@stochastik.uni-freiburg.de",
+     "groups": ["admin", "faq", "vvz"],
+     "kommentar": ""},
+    {"name": "Junker",
+     "vorname": "Markus",
+     "rz": "junker",
+     "email": "markus.junker@math.uni-freiburg.de",
+     "groups": ["admin", "faq", "vvz"],
+     "kommentar": ""},
+    {"name": "Rabe",
+     "vorname": "Ramona",
+     "rz": "rr1067",
+     "email": "ramona.rabe@math.uni-freiburg.de",
+     "groups": ["faq", "vvz"],
+     "kommentar": ""},
+    {"name": "Fix",
+     "vorname": "Daniel",
+     "rz": "df1050",
+     "email": "daniel.fix@math.uni-freiburg.de",
+     "groups": ["admin", "faq", "vvz"],
+     "kommentar": ""},
+    {"name": "Christoforidis",
+     "vorname": "Ioannis",
+     "rz": "ie1011",
+     "email": "ioannis.christoforidis@math.uni-freiburg.de",
+     "groups": ["admin", "faq", "vvz"],
+     "kommentar": ""},
+    {"name": "Hofmann",
+     "vorname": "Elias",
+     "rz": "eh1070",
+     "email": "elias.hofmann@math.uni-freiburg.",
+     "groups": ["admin", "faq", "vvz"],
+     "kommentar": ""},
+    {"name": "Caffier",
+     "vorname": "Anne",
+     "rz": "ac1074",
+     "email": "anne.caffier@math.uni-freiburg.de",
+     "groups": ["faq", "vvz"],
+     "kommentar": ""},
+    {"name": "Nunzio",
+     "vorname": "Esther di",
+     "rz": "ed1034",
+     "email": "esther.di.nunzio@math.uni-freiburg.de",
+     "groups": ["faq", "vvz"],
+     "kommentar": ""}]
+
+print("Dieser Vorgang löscht alle Einträge in user und group!")
+print("Wirklch fortfahren? [y/N]")
+input = input()
+if input == "y":
+    group.delete_many({})
+    user.delete_many({})
+    for x in groups:
+        group.insert_one(x)
+    for x in users:
+        user.insert_one(x)
+
+print([user["email"] for user in list(user.find({}))])
