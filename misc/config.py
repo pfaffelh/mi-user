0c0be37362a31f7061d0133faf68a14b71c7a936
pfaffelh <p.p@stochastik.uni-freiburg.de>
2024-03-06 21:54:31 +0100

    copied files from faq

---
+++ misc/config.py
@@ -0,0 +1,21 @@
+import pymongo
+
+# This is the ldap-server of the University, which is required for authentication
+server="ldaps://ldap.uni-freiburg.de"
+base_dn = "ou=people,dc=uni-freiburg,dc=de"
+
+import logging
+logging.basicConfig(level=logging.INFO, format = "%(asctime)s - %(levelname)s - schema - %(message)s")
+
+# Das ist die mongodb; 
+# Das Cluster users enthält User-Daten.
+# group enthält alle möglichen Gruppen für die verschiedenen Apps.
+# user enthält alle Nutzer.
+cluster = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+mongo_db_users = cluster["users"]
+user = mongo_db_users["user"]
+group = mongo_db_users["group"]
+
+logging.info("Connected to MongoDB")
+logging.info("Database contains collections: ")
+logging.info(str(mongo_db_users.list_collection_names()))
